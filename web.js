ac7e10f4324e3e0b45ed34e4edb9f3a9a9a392ea
Elias Shalabi <elias.sh87@gmail.com>
2013-07-20 10:23:46 +0000

    modified web.js with readFileSync

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.readFileSync('./index.html', function (err, data){
+	if (err) throw err;
+	console.log(data);
+});
+
+
 });
 
 var port = process.env.PORT || 5000;
