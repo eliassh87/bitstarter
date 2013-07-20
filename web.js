eb238474a26a2875494abd5038a22a1a7f667361
Elias Shalabi <elias.sh87@gmail.com>
2013-07-20 10:38:54 +0000

    4th with require(fs)

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var buff = fs.readFileSync("index.html");
 var app = express.createServer(express.logger());
 var content = buff.toString()
